58ccfc6eba6029ea478080495f5d20d00679209f
Magdalena Mordasewicz <magdalena.mordasewicz@intive.com>
2023-01-24 23:35:19 +0100

    first commit

---
+++ page_object/toDoListPageObject.ts
@@ -0,0 +1,40 @@
+import {expect} from '@playwright/test';
+
+
+const ulrTodolist = 'https://webdriveruniversity.com/To-Do-List/index.html'
+const newTodoPlaceholder = 'Add new todo';
+
+function delay(time) {
+    return new Promise(function(resolve) { 
+        setTimeout(resolve, time)
+    });
+ }
+
+export class ToDoListPageObject{
+    async navigate(page: any){
+        await page.goto(ulrTodolist)
+    }
+
+    async fillAddTodoItem (page: any, value: string){
+        await page.getByPlaceholder(newTodoPlaceholder).fill(value)
+    }
+
+    async enterAddTodoItem (page: any){
+        await page.getByPlaceholder(newTodoPlaceholder).press('Enter')
+    }
+
+    async checkIfTaskWasAdded (page: any, message: string){
+        await expect(page.locator(`text=${message}`)).toHaveText(message)
+    }
+
+    async clickOnCompletedTask (page: any, value: string){
+        await page.locator(`text=${value}`).click()
+    }
+    async chceckIfTaskIsCompleted (page: any, value: string){
+        await expect(page.locator(`text=${value}`)).toHaveClass('completed')
+        await expect(page.locator(`text=${value}`)).not.toHaveClass('')
+    }
+
+}
+
+export const toDoListPageObject = new ToDoListPageObject()
