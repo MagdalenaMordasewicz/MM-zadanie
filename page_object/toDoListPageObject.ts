cee60bc1e1207a9f5db3cfef7200606e3e74ef99
Magdalena Mordasewicz <magdalena.mordasewicz@intive.com>
2023-01-25 20:22:53 +0100

    Fix of hardcoded string and added test steps to tests

--- page_object/toDoListPageObject.ts
+++ page_object/toDoListPageObject.ts
@@ -3,13 +3,14 @@ import {expect} from '@playwright/test';
 const ulrTodolist = 'https://webdriveruniversity.com/To-Do-List/index.html'
 const newTodoPlaceholder = 'Add new todo';
 
+
 export class ToDoListPageObject{
     async navigate(page: any){
         await page.goto(ulrTodolist)
     }
 
-    async checkPageTitle (page: any){
-        await expect(page).toHaveTitle('WebDriver | To Do List');
+    async checkPageTitle (page: any, value: string){
+        await expect(page).toHaveTitle(value);
    }
 
     async fillAddTodoItem (page: any, value: string){
@@ -27,13 +28,13 @@ export class ToDoListPageObject{
     async clickOnCompletedTask (page: any, value: string){
         await page.locator(`text=${value}`).click()
     }
-    async chceckIfTaskIsCompleted (page: any, value: string){
+    async checkIfTaskIsCompleted (page: any, value: string){
         await expect(page.locator(`text=${value}`)).toHaveClass('completed')
-        await expect(page.locator(`text=${value}`)).not.toHaveClass('')
+        //await expect(page.locator(`text=${value}`)).not.toHaveClass('')
     }
 
-    async checkHowManyElementsOnList (page: any, decimal: number){
-        await expect(page.locator('li')).toHaveCount(decimal);
+    async checkIfNumberOfTodosIsCorrect (page: any, listItemsCount: number){
+        await expect(page.locator('li')).toHaveCount(listItemsCount);
     }
     
     async clickDeleteButton (page: any, value: string){
--- tests/testToDoList.spec.ts
+++ tests/testToDoList.spec.ts
@@ -8,36 +8,45 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('has title', async ({ page }) => {
-  await toDoListPageObject.checkPageTitle(page)
+  await test.step ("Check if page has title", async () =>{
+    await toDoListPageObject.checkPageTitle(page, toDoListData.pageTitle)})
 });
 
 test('add to do item', async ({page}) => {
-  await toDoListPageObject.fillAddTodoItem(page, toDoListData.taskToAdd)
-  await toDoListPageObject.enterAddTodoItem(page)
-  await toDoListPageObject.checkIfTaskWasAdded(page, toDoListData.taskToAdd)
+  await test.step ("Add task to do list", async () =>{
+    await toDoListPageObject.fillAddTodoItem(page, toDoListData.taskToAdd)
+    await toDoListPageObject.enterAddTodoItem(page)})
+  await test.step ("Check if task was added", async () =>{
+    await toDoListPageObject.checkIfTaskWasAdded(page, toDoListData.taskToAdd)})
 })
 
 test('completed to do item', async ({page}) => {
-
-  await toDoListPageObject.fillAddTodoItem(page, toDoListData.taskToComplete)
-  await toDoListPageObject.enterAddTodoItem(page)
-  await toDoListPageObject.checkIfTaskWasAdded(page, toDoListData.taskToComplete)
-  await toDoListPageObject.clickOnCompletedTask(page, toDoListData.taskToComplete)
-  await toDoListPageObject.chceckIfTaskIsCompleted(page, toDoListData.taskToComplete)
+  await test.step ("Add task to do list", async () =>{
+    await toDoListPageObject.fillAddTodoItem(page, toDoListData.taskToComplete)
+    await toDoListPageObject.enterAddTodoItem(page)
+    await toDoListPageObject.checkIfTaskWasAdded(page, toDoListData.taskToComplete)})
+  await test.step ("Get it marked as completed", async () =>{
+    await toDoListPageObject.clickOnCompletedTask(page, toDoListData.taskToComplete)
+    await toDoListPageObject.checkIfTaskIsCompleted(page, toDoListData.taskToComplete)})
 })
 
 test('count tasks on list', async ({page}) => {
-  await toDoListPageObject.checkHowManyElementsOnList(page, 3)
-  await toDoListPageObject.fillAddTodoItem(page, toDoListData.taskToComplete)
-  await toDoListPageObject.enterAddTodoItem(page)
-  await toDoListPageObject.checkIfTaskWasAdded(page, toDoListData.taskToComplete)
-  await toDoListPageObject.checkHowManyElementsOnList(page, 4)
+  await test.step ("Check if number of items in todo list at start", async () =>{    
+    await toDoListPageObject.checkIfNumberOfTodosIsCorrect(page,3)})
+  await test.step ("Add task to do list", async () =>{
+    await toDoListPageObject.fillAddTodoItem(page, toDoListData.taskToAdd)
+    await toDoListPageObject.enterAddTodoItem(page)
+    await toDoListPageObject.checkIfTaskWasAdded(page, toDoListData.taskToAdd)})
+  await test.step ("Check if number of items in todo list is correct", async () =>{
+    await toDoListPageObject.checkIfNumberOfTodosIsCorrect(page, 4)})
 })
 
 test('delete to do item', async ({page}) => {
-  await toDoListPageObject.fillAddTodoItem(page, toDoListData.taskToDelete)
-  await toDoListPageObject.enterAddTodoItem(page)
-  await toDoListPageObject.checkIfTaskWasAdded(page, toDoListData.taskToDelete)
-  await toDoListPageObject.clickDeleteButton(page, toDoListData.taskToDelete)
-  await toDoListPageObject.checkIfTaskWasDeleted(page, toDoListData.taskToDelete)
+  await test.step ("Add task to do list", async () =>{
+    await toDoListPageObject.fillAddTodoItem(page, toDoListData.taskToDelete)
+    await toDoListPageObject.enterAddTodoItem(page)
+    await toDoListPageObject.checkIfTaskWasAdded(page, toDoListData.taskToDelete)})
+  await test.step ("Delete item", async () =>{
+    await toDoListPageObject.clickDeleteButton(page, toDoListData.taskToDelete)
+    await toDoListPageObject.checkIfTaskWasDeleted(page, toDoListData.taskToDelete)})
 })
