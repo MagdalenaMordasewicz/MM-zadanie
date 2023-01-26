b6cf35fab6a5f8d1bb8d4ca3ab5dfd4a94a186ac
Magdalena Mordasewicz <magdalena.mordasewicz@intive.com>
2023-01-26 10:12:07 +0100

    Change for Page type

--- page_object/toDoListPageObject.ts
+++ page_object/toDoListPageObject.ts
@@ -7,34 +7,34 @@ const iLocator = 'i'
 
 
 export class ToDoListPageObject{
-    async navigate(page: any){
+    async navigate(page: Page){
         await page.goto(ulrTodolist)
     }
 
-    async checkPageTitle (page: any, value: string){
+    async checkPageTitle (page: Page, value: string){
         await expect(page).toHaveTitle(value);
    }
 
-    async fillAddTodoItem (page: any, value: string){
+    async fillAddTodoItem (page: Page, value: string){
         await page.getByPlaceholder(newTodoPlaceholder).fill(value)
     }
 
-    async enterAddTodoItem (page: any){
+    async enterAddTodoItem (page: Page){
         await page.getByPlaceholder(newTodoPlaceholder).press('Enter')
     }
 
-    async checkIfTaskWasAdded (page: any, value: string){
+    async checkIfTaskWasAdded (page: Page, value: string){
         await expect(page.locator(`text=${value}`)).toHaveText(value)
     }
 
-    async clickOnCompletedTask (page: any, value: string){
+    async clickOnCompletedTask (page: Page, value: string){
         await page.locator(`text=${value}`).click()
     }
-    async checkIfTaskIsCompleted (page: any, value: string){
+    async checkIfTaskIsCompleted (page: Page, value: string){
         await expect(page.locator(`text=${value}`)).toHaveClass('completed')
     }
 
-    async checkIfNumberOfTodosIsCorrect (page: any, listItemsCount: number){
+    async checkIfNumberOfTodosIsCorrect (page: Page, listItemsCount: number){
         await expect(page.locator(liLocator)).toHaveCount(listItemsCount)
     }
 
@@ -43,11 +43,11 @@ export class ToDoListPageObject{
         return itemsNumber;
     }
     
-    async clickDeleteButton (page: any, value: string){
+    async clickDeleteButton (page: Page, value: string){
         await page.getByText(value).hover()
         await page.getByRole('listitem').filter({hasText: value}).locator(iLocator).click()
     } 
-    async checkIfTaskWasDeleted (page: any, value: string){
+    async checkIfTaskWasDeleted (page: Page, value: string){
         await expect(page.locator(`text=${value}`)).not.toBeVisible()
     } 
 }
