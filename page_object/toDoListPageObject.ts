a5973be3b1f5f556ffd89d713f7d0d0162fed0a7
Magdalena Mordasewicz <magdalena.mordasewicz@intive.com>
2023-01-26 12:01:04 +0100

    Added test steps

--- page_object/toDoListPageObject.ts
+++ page_object/toDoListPageObject.ts
@@ -1,4 +1,4 @@
-import {expect, Page} from '@playwright/test';
+import {expect,test, Page} from '@playwright/test';
 
 const ulrTodolist = 'https://webdriveruniversity.com/To-Do-List/index.html'
 const newTodoPlaceholder = 'Add new todo'
@@ -8,47 +8,58 @@ const iLocator = 'i'
 
 export class ToDoListPageObject{
     async navigate(page: Page){
-        await page.goto(ulrTodolist)
+        await test.step("Navigate to given url", async () => {
+        await page.goto(ulrTodolist)})
     }
 
-    async checkPageTitle (page: Page, value: string){
-        await expect(page).toHaveTitle(value);
+    async checkPageTitle (page: Page, value: string){       
+        await test.step("Check if page has title", async () => {
+        await expect(page).toHaveTitle(value)});
    }
 
     async fillAddTodoItem (page: Page, value: string){
-        await page.getByPlaceholder(newTodoPlaceholder).fill(value)
+        await test.step("Fill add to do text box", async () => {
+        await page.getByPlaceholder(newTodoPlaceholder).fill(value)})
     }
 
     async enterAddTodoItem (page: Page){
-        await page.getByPlaceholder(newTodoPlaceholder).press('Enter')
+        await test.step("Press enter", async () => {
+        await page.getByPlaceholder(newTodoPlaceholder).press('Enter')})
     }
 
     async checkIfTaskWasAdded (page: Page, value: string){
-        await expect(page.locator(`text=${value}`)).toHaveText(value)
+        await test.step("Check if task was added", async () => {
+        await expect(page.locator(`text=${value}`)).toHaveText(value)})
     }
 
     async clickOnCompletedTask (page: Page, value: string){
-        await page.locator(`text=${value}`).click()
+        await test.step("Click on completed task", async () => {
+        await page.locator(`text=${value}`).click()})
     }
     async checkIfTaskIsCompleted (page: Page, value: string){
-        await expect(page.locator(`text=${value}`)).toHaveClass('completed')
+        await test.step("Check if task was completed", async () => {
+        await expect(page.locator(`text=${value}`)).toHaveClass('completed')})
     }
 
     async checkIfNumberOfTodosIsCorrect (page: Page, listItemsCount: number){
-        await expect(page.locator(liLocator)).toHaveCount(listItemsCount)
+        await test.step("Check if amount of to do's is correct", async () => {
+        await expect(page.locator(liLocator)).toHaveCount(listItemsCount)})
     }
 
     async countItemsOnList (page:Page){
-        const itemsNumber = await page.locator(liLocator).count();
-        return itemsNumber;
+        const itemCount = await test.step("Count items on to do list", async () => {
+        return await page.locator(liLocator).count()})
+        return itemCount;
     }
     
     async clickDeleteButton (page: Page, value: string){
+        await test.step("Click delete button", async () => {
         await page.getByText(value).hover()
-        await page.getByRole('listitem').filter({hasText: value}).locator(iLocator).click()
+        await page.getByRole('listitem').filter({hasText: value}).locator(iLocator).click()})
     } 
     async checkIfTaskWasDeleted (page: Page, value: string){
-        await expect(page.locator(`text=${value}`)).not.toBeVisible()
+        await test.step("Check if task was deleted", async () => {
+        await expect(page.locator(`text=${value}`)).not.toBeVisible()})
     } 
 }
 
